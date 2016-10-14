f8121e7148ea6e678cd02d501b5418b18b4b11d5
chrxtina <christina.ad.nguyen@gmail.com>
2016-10-14 11:46:22 -0400

    Name file uploads using crypto module
    - Define wrapper functions for naming the file and directory
    - TIL: be really careful with Promises

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,38 +1,65 @@
 'use strict';
 
-// this has to come  before anything else
+// this has to come before anything else
 require('dotenv').config();
 
 const fs = require('fs');
+const crypto = require('crypto');
+
 const fileType = require('file-type');
 const AWS = require('aws-sdk');
 
 const filename = process.argv[2] || '';
 
 const readFile = (filename) => {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fs.readFile(filename, (error, data) => {
       if (error) {
         reject(error);
       }
 
-    resolve(data);
+      resolve(data);
     });
   });
 };
 
-// return a default object in case that fileType is given an unsupported
+// return a default object in the case that fileType is given an unsupported
 // filetype to read
 const mimeType = (data) => {
   return Object.assign({
     ext: 'bin',
     mime: 'application/octet-stream',
-  },fileType(data));
+  }, fileType(data));
 };
 
 const parseFile = (fileBuffer) => {
   let file = mimeType(fileBuffer);
   file.data = fileBuffer;
+  return file;
+};
+
+const randomHexString = (length) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (error, buffer) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(buffer.toString('hex'));
+    });
+  });
+};
+
+const nameFile = (file) => {
+  return randomHexString(16)
+  .then((val) => {
+    file.name = val;
+    return file;
+  });
+};
+
+const nameDirectory = (file) => {
+  file.dir = new Date().toISOString().split('T')[0];
   return file;
 };
 
@@ -45,36 +72,38 @@ const s3 = new AWS.S3({
 
 const upload = (file) => {
   const options = {
-    // Get bucket name from AWS console
+    // get the bucket name from your AWS S3 console
     Bucket: 'silentbucket',
-    // Attach fileBuffer as a stream to send to S3
+    // attach the fileBuffer as a stream to send to S3
     Body: file.data,
-    // Allow anyone to access the URL of the uploaded file
+    // allow anyone to access the URL of the uploaded file
     ACL: 'public-read',
-    // Tell S3 what the mime-type is
+    // tell S3 what the mime-type is
     ContentType: file.mime,
-    // Pick a filename for S3 to use for the upload
-    Key: `test/test.${file.ext}`
+    // pick a filename for S3 to use for the upload
+    Key: `${file.dir}/${file.name}.${file.ext}`
   };
-  // Don't upload yet, just pass data down Promise chain
-  // return Promise.resolve(options);
-  return new Promise ((resolve, reject)=>{
-    s3.upload(options, (error, data)=>{
-      if (error){
+
+  return new Promise((resolve, reject) => {
+    s3.upload(options, (error, data) => {
+      if (error) {
         reject(error);
       }
-      resolve (data);
+
+      resolve(data);
     });
   });
 };
 
 const logMessage = (response) => {
-  // Turn pojo into a string to see in console
-  console.log(`the response from AWS was options ${JSON.stringify(response)}`);
+  // turn the pojo into a string so I can see it on the console
+  console.log(`the response from AWS was ${JSON.stringify(response)}`);
 };
 
 readFile(filename)
 .then(parseFile)
+.then(nameFile)
+.then(nameDirectory)
 .then(upload)
 .then(logMessage)
 .catch(console.error)
